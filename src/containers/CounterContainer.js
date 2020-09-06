7ff2ba51e1c8cba46b0f2b4277b825e8d6f9d3f8
Logan <tkdgk3817@gmail.com>
2020-09-06 17:05:33 +0900

    learn redux-actions

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,27 +1,40 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+// import { connect } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux'
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+    
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    
     return (
-        <Counter number={ number } onIncrease={ increase } onDecrease={ decrease } />
+        <Counter 
+            number={ number }
+            onIncrease={ onIncrease }
+            onDecrease={ onDecrease }
+        />
+        // <Counter number={ number } onIncrease={ increase } onDecrease={ decrease } />
     );
 }
 
-const mapStateToProps = state => ({
-    number: state.counter.number
-});
+// const mapStateToProps = state => ({
+//     number: state.counter.number
+// });
 
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        dispatch(increase());
-        console.log("increase");
-    },
-    decrease: () => {
-        dispatch(decrease());
-        console.log("decrease");
-    }
-});
+// const mapDispatchToProps = dispatch => ({
+//     increase: () => {
+//         dispatch(increase());
+//         console.log("increase");
+//     },
+//     decrease: () => {
+//         dispatch(decrease());
+//         console.log("decrease");
+//     }
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -3,8 +3,8 @@ import { createAction, handleActions } from 'redux-actions';
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
-export const increase = () => createAction(INCREASE);
-export const decrease = () => createAction(DECREASE);
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
     number: 0
@@ -12,8 +12,8 @@ const initialState = {
 
 const counter = handleActions(
     {
-        [INCREASE]: (state, action) => ({ nunber: state.nunber + 1 }),
-        [DECREASE]: (state, action) => ({ number: state.nunber - 1 })
+        [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+        [DECREASE]: (state, action) => ({ number: state.number - 1 }),
     },
     initialState
 );
